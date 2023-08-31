6fc5c3318a82f28dcc803af22bdfe6fd032d5322
sayedabdelal <saidmo19734@gmail.com>
2023-08-31 04:30:34 -0700

    chore

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,21 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * @binary_tree_node: function that bienry nde.
- * @parent:parent is a pointer to the parent node of the node.
- * @value: value to put in the new node.
- * Return: the newly created node.
- */
+ * binary_tree_node - build a binary tree node.
+ * @parent: A pointer to the parent of the node.
+ * @value: The value of node.
+ *
+ * Return: If an error occurs - NULL.
+*/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *n_binery_nde;
+	binary_tree_t *n_binery_nde;
 
 	n_binery_nde = malloc(sizeof(binary_tree_t));
 
 	if (n_binery_nde == NULL)
 		return (NULL);
-	
 	n_binery_nde->n = value;
 	n_binery_nde->parent = parent;
 	n_binery_nde->left = NULL;
