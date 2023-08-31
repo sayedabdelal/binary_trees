a8fe26472e9b66f6beb7a6e9b8533e3493808340
sayedabdelal <saidmo19734@gmail.com>
2023-08-31 05:35:18 -0700

    chore

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - take height of a binary tree
+ * @tree: pointer to tree node of height
+ * Return: NULL or height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l_height, r_height;
+
+	if (!tree)
+		return (0);
+
+	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return (l_height > r_height ? l_height : r_height);
+}
